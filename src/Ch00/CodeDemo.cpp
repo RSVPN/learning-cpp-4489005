2f585d48ada3e11ecafba03ec586a94389f5691d
RSVPN <114849139+RSVPN@users.noreply.github.com>
2024-06-24 11:14:00 +0000

    comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Learning C++ 
-// Exercise 00_03
+// Exercise 00_03 with modifications
 // Using the exercise files on GitHub, by Eduardo Corpeño 
 
 #include <iostream>
@@ -9,6 +9,7 @@ int main(){
 
     std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
+    
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
     
